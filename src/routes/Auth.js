3b90160d52f268d9ace10592f722a6fd35f3936f
wonjinYi <studioplug17@gmail.com>
2021-07-25 22:43:10 +0900

    basic auth layout.

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth =  () => <span>auth</span>
+const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    }
+
+    return {
+        value,
+        onChange,
+    }
+}
+
+const Auth = () => {
+    const email = useInput('');
+    const password = useInput('');
+
+    const onSubmit = () => {
+        console.log('submit')
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="Email" required {...email} />
+                <input type="password" placeholder="password" required {...password} />
+                <input type="submit" value="Login" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Gighub</button>
+            </div>
+        </div>
+    )
+}
 
 export default Auth;
