18711e9cef925714c3cc7306d280fa7d8ce62336
wonjinYi <studioplug17@gmail.com>
2021-07-26 00:30:53 +0900

    oAuth sign in for google, github. But not yet logout.

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from 'fBase';
+import { authService, firebaseInstance } from 'fBase';
 import React, { useState } from 'react';
 
 const useInput = (initialValue) => {
@@ -42,6 +42,28 @@ const Auth = () => {
     const toggleAccount = () => {
         setNewAccount( prev => !prev );
     }
+
+    const onSocialClick = async (event) => {
+        const name = event.target.name;
+        
+        let provider;
+        if(name === 'google'){
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+            provider.setCustomParameters({
+                prompt: 'select_account'
+            });
+        } else if(name === 'github'){
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+            provider.setCustomParameters({
+                login : 'true'
+            });
+        }
+
+
+        const data = await authService.signInWithPopup(provider);
+        console.log(data);
+    }
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -52,8 +74,8 @@ const Auth = () => {
             </form>
             <span onClick={toggleAccount}>{newAccount ? 'Sign in' : 'CreateAccount'}</span>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Gighub</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
+                <button onClick={onSocialClick} name="github">Continue with Github</button>
             </div>
         </div>
     )
