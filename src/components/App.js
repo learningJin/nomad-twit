776867c8c491b6e34ab2d9288efe5f60b61c89fc
wonjinYi <studioplug17@gmail.com>
2021-07-28 22:44:53 +0900

    delete state 'isLoggedIn'

--- src/components/App.js
+++ src/components/App.js
@@ -5,16 +5,12 @@ import AppRouter from 'components/Router';
 
 function App() {
   const [init, setInit] = useState(false); // Is this component initialized?
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
 
   useEffect( () => {
     authService.onAuthStateChanged( user => {
       if(user){
-        setIsLoggedIn(true);
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
       }
       setInit(true)
     });
@@ -22,7 +18,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : 'initializing'}
+      {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} /> : 'initializing'}
       <footer>&copy; {new Date().getFullYear()} </footer>
     </>
   );
